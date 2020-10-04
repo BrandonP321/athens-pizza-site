36d35d6def1d05f0d7759df2669b6ba02359bb05
Brandon Phillips <brandon.phillips976@gmail.com>
2020-10-03 22:56:34 -0700

    added extra styling and scroll when btn is clicked

--- menu.js
+++ menu.js
@@ -4,10 +4,11 @@ $('.btn').on('click', function () {
     const btnClicked = $(this)
     const allMenuOptionsDiv = $('#allMenuOptionsDiv')
     const isExpanded = allMenuOptionsDiv.attr('data-menuExpanded')
-    console.log('isExpanded: ' + isExpanded)
 
     // only change menu options display if the clicked btn has the data-menu attribute
     if (btnClicked.attr('data-menu')) {
+        $(window).scrollTop(0)
+
         menuToDisplay = btnClicked.attr('data-menu')
 
         if (isExpanded == 'true') {
@@ -22,13 +23,8 @@ $('.btn').on('click', function () {
 })
 
 $('#allMenuOptionsDiv').on('hidden.bs.collapse', function () {
-    console.log('hidden')
     // run collapse('show') now that the div is hidden
     $('.menuOptions').css('display', 'none')
     $(menuToDisplay).css('display', 'block')
     $('#allMenuOptionsDiv').collapse('show')
-})
-
-$('#allMenuOptionsDiv').on('shown.bs.collapse', function() {
-    console.log('shown')
 })
