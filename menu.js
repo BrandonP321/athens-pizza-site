c7853ae9bf8281c62df02e02f54f656a9c759805
Brandon Phillips <brandon.phillips976@gmail.com>
2020-10-03 20:41:00 -0700

    added functionality to collapse menu options div

--- menu.js
+++ menu.js
@@ -1,46 +1,34 @@
+let menuToDisplay;
 
-// add scroll to mene dropdown when it is clicked
-$('.btn').on('click', function(event) {
-    // store clicked btn to be accessed later
-    var currentEle = $(this)
+$('.btn').on('click', function () {
+    const btnClicked = $(this)
+    const allMenuOptionsDiv = $('#allMenuOptionsDiv')
+    const isExpanded = allMenuOptionsDiv.attr('data-menuExpanded')
+    console.log('isExpanded: ' + isExpanded)
 
-    if (currentEle)
-    // wait 350ms to allow bootstrap animation to finish loading
-    setTimeout(function(){
-        // get offset from top of screen of clicked element
-        var offset = currentEle.offset()
-        // remove 90 more pixels to account to space of navbar
-        offset.top -= 90
-        // scroll to have element 90 pixels from top of screen
-        $('html, body').animate({
-            scrollTop: offset.top
-        }, 0, 'swing')
-    }, 300)
+    // only change menu options display if the clicked btn has the data-menu attribute
+    if (btnClicked.attr('data-menu')) {
+        menuToDisplay = btnClicked.attr('data-menu')
+
+        if (isExpanded == 'true') {
+            allMenuOptionsDiv.collapse('hide')
+        } else {
+            allMenuOptionsDiv.attr('data-menuExpanded', 'true')
+            $('.menuOptions').css('display', 'none')
+            $(menuToDisplay).css('display', 'block')
+            allMenuOptionsDiv.collapse('show')
+        }
+    }
 })
 
-// click event for accordion buttons in menu
-// $('.btn').on('click', function() {
-//     // retrieve +/- sign showing if menu is dropped down or not
-//     const dropDownSign = $(this).children('.dropdownSign')
+$('#allMenuOptionsDiv').on('hidden.bs.collapse', function () {
+    console.log('hidden')
+    // run collapse('show') now that the div is hidden
+    $('.menuOptions').css('display', 'none')
+    $(menuToDisplay).css('display', 'block')
+    $('#allMenuOptionsDiv').collapse('show')
+})
 
-//     // if current menu is not dropped and showing a '+', change it to a '-'
-//     if (dropDownSign.attr('data-isShowing') === 'false') {
-//         dropDownSign.html('&minus;')
-//         dropDownSign.attr('data-isShowing', 'true')
-//     } else {
-//         dropDownSign.html('&plus;')
-//         dropDownSign.attr('data-isShowing', 'false')
-//     }
-
-//     // change any other plus/minus signs not related to the current menu to plus
-//     var allDropDowns = $('.dropdownSign')
-//     var currentDropDown = dropDownSign[0]
-//     console.log(allDropDowns)
-//     for (let dropdown of allDropDowns) {
-//         console.log(dropdown)
-//         console.log(dropDownSign)
-//         if (dropdown === dropDownSign) {
-//             console.log('nope')
-//         }
-//     }
-// })
+$('#allMenuOptionsDiv').on('shown.bs.collapse', function() {
+    console.log('shown')
+})
