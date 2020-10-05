bc7fa537bf9c748bb9451a0104ff52508fdec732
Brandon Phillips <brandon.phillips976@gmail.com>
2020-10-04 20:30:27 -0700

    added menu button for responsive menu on smaller screens

--- menu.js
+++ menu.js
@@ -1,4 +1,25 @@
 let menuToDisplay;
+let menuIsCenter = false;
+
+function menuToCenter() {
+    const menuPopBtn = $('.menuPopBtn')
+    const menuAside = $('.foodMenusAside')
+
+    menuPopBtn.animate({right: '55%'})
+    menuAside.animate({right: 0})
+
+    menuIsCenter = true
+}
+
+function menuToOffScreen() {
+    const menuPopBtn = $('.menuPopBtn')
+    const menuAside = $('.foodMenusAside')
+
+    menuPopBtn.animate({right: "-5%"})
+    menuAside.animate({right: '-60vw'})
+
+    menuIsCenter = false
+}
 
 $('.btn').on('click', function () {
     const btnClicked = $(this)
@@ -7,6 +28,11 @@ $('.btn').on('click', function () {
 
     // only change menu options display if the clicked btn has the data-menu attribute
     if (btnClicked.attr('data-menu')) {
+
+        if ($(window).width() <= 900) {
+            menuToOffScreen()
+        }
+
         $(window).scrollTop(0)
 
         menuToDisplay = btnClicked.attr('data-menu')
@@ -27,4 +53,12 @@ $('#allMenuOptionsDiv').on('hidden.bs.collapse', function () {
     $('.menuOptions').css('display', 'none')
     $(menuToDisplay).css('display', 'block')
     $('#allMenuOptionsDiv').collapse('show')
+})
+
+$('.menuPopBtn').on('click', function() {
+    if (menuIsCenter) {
+        menuToOffScreen()
+    } else {
+        menuToCenter()
+    }
 })
